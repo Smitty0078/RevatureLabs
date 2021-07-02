44ac523e78e4420d1b4817da0a6d043c36f5ddb4
Smitty0078 <smitty0078@gmail.com>
2021-07-02 00:43:25 -0400

    Adding Labs
    Labs day 2 - day 4

---
+++ Lab-Datatypes/src/VariableDeclaration.java
@@ -0,0 +1,24 @@
+
+public class VariableDeclaration {
+
+	public static void main(String[] args) {
+		boolean on = false;
+		System.out.println(on);
+		
+		short s = 16;
+		System.out.println(s);
+		
+		int i = -32;
+		System.out.println(i);
+		
+		float f = 3839.34839f;
+		System.out.println(f);
+		
+		long L = 4_294_967_296L;
+		System.out.println(L);
+		
+		char c = 'a';
+		System.out.println(c);
+	}
+
+}
---
+++ Lab-DoWhile/src/ExampleOne.java
@@ -0,0 +1,15 @@
+
+public class ExampleOne {
+
+	public static void main(String[] args) {
+		boolean on = false;
+		
+		do 
+		{
+			System.out.println("inside do while loop");
+		}while(on);
+		
+		
+	}
+
+}
