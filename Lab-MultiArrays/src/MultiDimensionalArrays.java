c8419c8bf03bd3d3513e8c9ea5f21eeea7f1b9aa
Smitty0078 <smitty0078@gmail.com>
2021-07-02 00:52:52 -0400

    finished multidim array
    lab for multidimensional arrays

--- Lab-MultiArrays/src/MultiDimensionalArrays.java
+++ Lab-MultiArrays/src/MultiDimensionalArrays.java
@@ -7,6 +7,11 @@ public class MultiDimensionalArrays {
 		
 		System.out.println(my3dArray[0][0][0]);
 		
+		int[][] my2dArray = {{1,2,0}, {4,5,6}, {1,4,5}};
+		System.out.println(my2dArray.length);
+		
+		System.out.println(my2dArray[1][2]);
+		
 	}
 
 }
