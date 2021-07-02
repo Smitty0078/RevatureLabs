9b9a546d9a9ed167933c503956f173e467e4b382
Smitty0078 <smitty0078@gmail.com>
2021-07-02 16:02:08 -0400

    Finished String Methods Lab

---
+++ Lab-StringMethods/src/StringMethods.java
@@ -0,0 +1,18 @@
+
+public class StringMethods {
+
+	public static void main(String[] args) {
+		String str = "Hello";
+		System.out.println(str.equals("Hello"));
+		
+		String s = new String("Hello");
+		System.out.println(str.equals(s));
+		
+		System.out.println(str.length());
+		
+		System.out.println(str.indexOf("H"));
+		
+
+	}
+
+}
