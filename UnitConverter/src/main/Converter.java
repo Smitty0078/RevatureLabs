508165a1e5a4e4052a3080267eae469b7fa7e491
Smitty0078 <smitty0078@gmail.com>
2021-07-05 21:57:36 -0400

    updated unit conversion project

--- UnitConverter/src/main/Converter.java
+++ UnitConverter/src/main/Converter.java
@@ -7,14 +7,9 @@ public class Converter {
 	public double num;
 	public double convertedNum;
 	
-	public static void main(String[] args) {
-		//initialize constant conversion variables
-		final double cupsToTeaspoons = 48.0;
-		final double milesToKilometers = 1.60934;
-		final double gallonsToImperialGallons = 0.832674;
-		
+	public static void main(String[] args) {	
 		//initialize variables
-		int conversionType = 0;
+		int menuSelection = 0;
 		boolean askForInput = true;
 		Scanner scan = new Scanner(System.in);
 		Converter convert = new Converter();
@@ -22,44 +17,31 @@ public class Converter {
 		//drives data converter
 		while(askForInput)
 		{
+			System.out.println("Welcome to the main Unit Converter!"+'\n');
 			//Ask user to input conversion type
 			System.out.println("Please select an option:"+'\n'
-		                      +"1 : Cups to Teaspoons"+'\n'
-							  +"2 : Miles to Kilometers"+'\n'
-		                      +"3 : US Gallons to Imperial Gallons"+'\n'
-							  +"4 : Quit"
+		                      +"1 : Volume Conversions"+'\n'
+							  +"2 : Distance Conversions"+'\n'
+		                      +"3 : Quit"+'\n'
 							  +'\n');
 			
 			//allows user to input an integer value to select which converter to use
-			conversionType = scan.nextInt();
+			menuSelection = scan.nextInt();
 			
 			//uses input to call the correct converter
-			switch(conversionType) 
+			switch(menuSelection) 
 			{
-            	case 1: {
-            		
-            		System.out.println("Please Enter Number of Cups: ");
-            		convert.num = scan.nextDouble();
-            		convert.converter(cupsToTeaspoons, "cup", "teaspoon");
-            	
+            	case 1: 
+            	{
+            		convert.volumeConversions(scan);
             		break;
             	}
-            	case 2:{
-            		System.out.println("Please Enter Number of Miles: ");
-            		convert.num = scan.nextDouble();
-            		convert.converter(milesToKilometers, "mile", "kilometer");
-            		
+            	case 2:
+            	{
+            		convert.distanceConversions(scan);
             		break;
             	}
             	case 3:
-            	{
-            		System.out.println("Please Enter Number of Gallons: ");
-            		convert.num = scan.nextDouble();
-            		convert.converter(gallonsToImperialGallons, "gallon", "imperial gallon");
-            		
-            		break;
-            	}
-            	case 4:
             	{
             		System.out.println("Thank you for using our Unit Converter"+'\n'+"Good bye :)");
             		askForInput = false;
@@ -79,11 +61,155 @@ public class Converter {
 
 	}
 	
-	public void converter(double conversionRatio, String type1, String type2)
+	//------------------------------------------------------------------------------------------------------
+	//Collects input for conversion
+	private static double collectQuantity(String unit, Scanner scan)
 	{
-		convertedNum = num * conversionRatio;
-		System.out.println(num+" "+type1+"(s) is equal to "+convertedNum+" "+type2+"(s)."+'\n');
+		double quantity = 0.0;
+		System.out.println("Please Enter Number of "+unit+": ");
+		quantity = scan.nextDouble();
+		
+		return quantity;
 	}
 	
+	//------------------------------------------------------------------------------------------------------
+	
+	public void volumeConversions(Scanner scan)
+	{
+		boolean askForInput = true;
+		int menuSelection = 0;
+		while(askForInput)
+		{
+			System.out.println("Welcome to the Volume Unit Converter!"+'\n');
+			System.out.println("Please select an option:"+'\n'
+							 +"1 : Cups to Teaspoons"+'\n'
+							 +"2 : Teaspoons to Tablespoons"+'\n'
+							 +"3 : US Gallons to Imperial Gallons"+'\n'
+							 +"4 : Quit"
+							 +'\n');
+			menuSelection = scan.nextInt();
+			switch(menuSelection) 
+			{
+            	case 1: 
+            	{
+            		num = collectQuantity("Cups", scan);
+            		cupsToTeaspoonsConverter();
+            		break;
+            	}
+            	case 2:
+            	{
+            		num = collectQuantity("Teaspoons", scan);
+            		teaspoonsToTablespoons();
+            		break;
+            	}
+            	case 3:
+            	{
+            		num = collectQuantity("Gallons", scan);
+            		gallonsToImperialGallons();
+            		break;
+            	}
+            	case 4:
+            	{
+            		System.out.println("Thank you for using our Volume Converter"+'\n');
+            		askForInput = false;
+            		break;
+            	}
+            	default:
+            	{
+            		System.out.println("Invalid Input");
+            		break;
+            	}
+			}
+	
+		}
+	}
+	
+	public void distanceConversions(Scanner scan)
+	{
+		boolean askForInput = true;
+		int menuSelection = 0;
+		System.out.println("Welcome to the Distance Unit Converter!"+'\n');
+		while(askForInput)
+		{
+			System.out.println("Please select an option:"+'\n'
+							 +"1 : Feet to Meters"+'\n'
+							 +"2 : Miles to Kilometers"+'\n'
+							 +"3 : Quit"
+							 +'\n');
+			menuSelection = scan.nextInt();
+			switch(menuSelection) 
+			{
+            	case 1: 
+            	{
+            		num = collectQuantity("Feet", scan);
+            		feetToMeters();
+            		break;
+            	}
+            	case 2:
+            	{
+            		num = collectQuantity("Miles", scan);
+            		milesToKilometers();
+            		break;
+            	}
+            	case 3:
+            	{
+            		System.out.println("Thank you for using our Distance Converter"+'\n');
+            		askForInput = false;
+            		break;
+            	}
+            	default:
+            	{
+            		System.out.println("Invalid Input");
+            		break;
+            	}
+            		
+			}
+		}
+	}
+	
+	public void teaspoonsToTablespoons()
+	{
+		//Conversion ratio: 1 teaspoon = 0.333333 tablespoons
+		convertedNum = num * 0.333333;
+		System.out.println(num+" teaspoon(s) is equal to "+convertedNum+" tablespoon(s)."+'\n');
+	}
+	
+	//------------------------------------------------------------------------------------------------------
+	//Drives conversion for cups and teaspoons
+	public void cupsToTeaspoonsConverter()
+	{	
+		//Conversion ratio: 1 Cup = 48 Teaspoons
+		convertedNum = num * 48;
+		System.out.println(num+" cup(s) is equal to "+convertedNum+" teaspoon(s)."+'\n');
+	}
+	
+	//------------------------------------------------------------------------------------------------------
+	//Drives conversion for gallons to imperial gallons
+	public void gallonsToImperialGallons()
+	{
+		//conversion ratio: 1 Gallon = 0.832674 Imperial Gallons
+		convertedNum = num * 0.832674;
+		System.out.println(num+" gallon(s) is equal to "+convertedNum+" imperial gallon(s)"+'\n');
+	}
+	
+	//------------------------------------------------------------------------------------------------------
+	//Drives conversion for miles and kilometers
+	public void milesToKilometers()
+	{
+		//Conversion ratio: 1 Mile = 1.60934 Kilometers
+		convertedNum = num * 1.60934;
+		System.out.println(num + " mile(s) is equal to "+convertedNum+" kilometer(s)."+'\n');
+	}
+	
+	//------------------------------------------------------------------------------------------------------
+	public void feetToMeters()
+	{
+		//Conversion ratio: 1 foot = 0.3048 Meters
+		convertedNum = num * 0.3048;
+		if(num == 1.0)
+			System.out.println(num + " foot is equal to "+convertedNum+" meter(s)."+'\n');
+		else
+			System.out.println(num + " feet is equal to "+convertedNum+" meter(s)."+'\n');
+	}
 
 }
