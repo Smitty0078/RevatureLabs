079ba80d219891ae0a680ecbc91424909a25f2e9
Smitty0078 <smitty0078@gmail.com>
2021-08-12 11:56:14 -0400

    added create account component

--- project_01_ERS/angular-front-end/src/app/app.module.ts
+++ project_01_ERS/angular-front-end/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { EmployeeSigninComponent } from './employee-signin/employee-signin.component';
+import { CreateNewAccountComponent } from './create-new-account/create-new-account.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EmployeeSigninComponent
+    EmployeeSigninComponent,
+    CreateNewAccountComponent
   ],
   imports: [
     BrowserModule
